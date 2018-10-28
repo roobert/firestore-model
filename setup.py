6b7e84b4f6669e6af777055d100cdb1a3d7b4d17
jeremyfromearth <jeremynealbrown@gmail.com>
2018-10-28 14:57:09 -0700

    Updated description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
   long_description = fh.read()
 
 setuptools.setup(
-  name="firestore-model",
+  name="firestore_model",
   version="0.0.1",
   author="Future Projects",
   author_email="info@futureprojects.io",
