34dfb64f99a02eb76f5dcbdbd8a0370b7d1bf28f
jeremyfromearth <jeremynealbrown@gmail.com>
2018-10-28 20:29:22 -0700

    Added example

---
+++ examples/main.py
@@ -0,0 +1,45 @@
+import firestore_model
+from dataclasses import dataclass
+from firestore_model import Model
+from google.cloud import firestore
+
+# Initialize connection to the database
+firestore_model.db = firestore.Client()
+
+# Create a model class
+@dataclass
+class Book(Model):
+  title:str
+  author:str
+  publisher:str
+  year:int 
+  pages:int
+
+
+if __name__ == '__main__':
+  book_id = '100800604002'
+
+  b = Book.make(
+    title='Sirens of Titan',
+    author='Kurt Vonnegut',
+    publisher='Delacorte', 
+    year=1959, 
+    pages=319
+  )
+
+  b.id = book_id
+  print(b.to_dict())
+  b.save()
+
+  b = Book.get(book_id)
+  print(b)
+
+  query = Book.query([
+    ('author', 'Kurt Vonnegut'),
+  ])
+
+  for b in query.get():
+    print(b.to_dict())
+
+  b.delete()
+  print(Book.get(book_id))
