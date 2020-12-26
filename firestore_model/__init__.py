92e91e404740daae334bf1c0478b6af4ef83bf1b
Rob Wilson <roobert@gmail.com>
2020-12-26 23:25:44 +0000

    Add collection support

--- firestore_model/__init__.py
+++ firestore_model/__init__.py
@@ -1,7 +1,7 @@
-import uuid
 import functools
+import uuid
+from dataclasses import asdict, dataclass
 from time import time
-from dataclasses import dataclass, asdict
 
 # --------------------------------------------
 #
@@ -10,29 +10,34 @@ from dataclasses import dataclass, asdict
 # -------------------------------------------
 db = None
 
+
 # --------------------------------------------
 #
 #  Utils
 #
 # -------------------------------------------
 def get_milliseconds():
-  """
+    """
     @return Milliseconds since the epoch
-  """
-  return int(round(time() * 1000))
+    """
+    return int(round(time() * 1000))
+
 
 def require_database(f, *args, **kwargs):
-  """ Decorator for methods that access the database
+    """ Decorator for methods that access the database
     @raises Exception
     @return Decorator for methods that require database access
-  """
-  @functools.wraps(f)
-  def wrapper(*args, **kwargs):
-    if db is None:
-      raise Exception('Database is not defined.')
-    else:
-      return f(*args, **kwargs)
-  return wrapper
+    """
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        if db is None:
+            raise Exception("Database is not defined.")
+        else:
+            return f(*args, **kwargs)
+
+    return wrapper
+
 
 # --------------------------------------------
 #
@@ -40,169 +45,235 @@ def require_database(f, *args, **kwargs):
 #
 # -------------------------------------------
 
+
 class Query(object):
-  """ A class representing a query on a collection
-  """
-  def __init__(self, cls, query_params):
+    """ A class representing a query on a collection
     """
-      @param cls The model class to run the query on
-      @param A list of query params. The lists can be (key, value) or (key, operator, value)
 
-      While possible, this method is not intended to be called by itself. The intended use
-      is from within the Model.query method.
+    def __init__(self, cls, query_params, collection_path: str = None):
+        """
+        @param cls
+            The model class to run the query on
+        @param query_params
+            A list of query params. The lists can be (key, value) or (key, operator,
+            value)
+        @param collection_path
+            Override default collection
 
-      Examples:
-        # Get all users with first name Sonic
-        q = User.query([('first_name', 'Sonic')])
-        result = q.get()
-        for r in result:
-          # do something with r
+        While possible, this method is not intended to be called by itself. The intended
+        use is from within the Model.query method.
 
-        # Get 10 users created before a specific datetime
-        query = User.query([('created', '<', 23409328408)])
-        query.q.limit(10)
-        result = query.get()
-        for r in result:
-          # do something with r
+        Examples:
+          # Get all users with first name Sonic
+          q = User.query([('first_name', 'Sonic')])
+          result = q.get()
+          for r in result:
+            # do something with r
 
-      References:
-        https://googleapis.github.io/google-cloud-python/latest/firestore/query.html
-    """
-    self.cls = cls
-    self.q = db.collection(cls.__name__)
+          # Get 10 users created before a specific datetime
+          query = User.query([('created', '<', 23409328408)])
+          query.q.limit(10)
+          result = query.get()
+          for r in result:
+            # do something with r
 
-    # parse the params
-    for param in query_params:
-      if len(param) == 2:
-        self.q = self.q.where(param[0], '==', param[1])
-      if len(param) == 3:
-        self.q = self.q.where(*param)
+        References:
+          https://googleapis.github.io/google-cloud-python/latest/firestore/query.html
+        """
+        self.cls = cls
+        self.result = None
 
-  def get(self, limit=None):
-    """ Executes the query
-      @return Generator object that yields hydrated instances of the class supplied __init__
-    """
-    if limit:
-      self.q.limit(limit)
+        self.q = (
+            db.collection(collection_path)
+            if collection_path
+            else db.collection(cls.__name__)
+        )
 
-    for doc in self.q.stream():
-      doc_data = doc.get(field_path=None)
-      if hasattr(self.cls, 'from_dict'):
-        yield self.cls.from_dict(doc_data)
-      else:
-        yield self.cls(**doc_data)
+        # parse the params
+        for param in query_params:
+            if len(param) == 2:
+                self.q = self.q.where(param[0], "==", param[1])
+            if len(param) == 3:
+                self.q = self.q.where(*param)
+
+    def get(self):
+        """ Executes the query
+        @return Generator
+            Generator object that yields hydrated instances of the class supplied
+        __init__
+        """
+        self.result = self.q.get()
+        for r in self.result:
+            if hasattr(self.cls, "from_dict"):
+                yield self.cls.from_dict(r.to_dict())
+            else:
+                yield self.cls(**r.to_dict())
+
 
 @dataclass
 class Model:
-  """ Base class for all other model classes
-  """
-  # --------------------------------------------
-  #
-  #  static
-  #
-  # -------------------------------------------
-
-  @classmethod
-  @require_database
-  def delete_doc(cls, doc_id):
-    try:
-      result = db.collection(cls.__name__).document(doc_id).delete()
-    except Exception as e:
-      print(e)
-
-  @classmethod
-  @require_database
-  def get(cls, doc_id, raise_exception=False):
-    """ Get a single model instance
-      @param cls The class of the instance calling make
-      @param doc_id The id of the document to get
-      @return A model instance of type class hydrated w/ data from the database
+    """ Base class for all other model classes
     """
-    try:
-      doc_ref = db.collection(cls.__name__).document(doc_id).get()
-      return cls(**doc_ref.to_dict())
-    except Exception as e:
-      if raise_exception:
-        raise e
-      return None
 
-  @classmethod
-  def make(cls, doc_id=None, save=False, *args, **kwargs): 
-    """ Create a new instance of a model class
-      @param cls The class of the instance calling make
-      @param doc_id Allow create document with a custom id
-      @param save A flag indicating the model should be saved immediately after creation
-      @returns A new model instance of type cls
+    # --------------------------------------------
+    #
+    #  static
+    #
+    # -------------------------------------------
 
-      Example:
-        User.make(
-            name = 'Sonic',
-            location = 'Earth',
-            save = True
-          )
-    """
-    id_str = doc_id if doc_id else str(uuid.uuid4())
-    created = get_milliseconds()
-    m = cls(id_str, created, created, *args, **kwargs)
-    if save: m.save()
-    return m
+    @classmethod
+    @require_database
+    def delete_doc(cls, doc_id, collection_path: str = None):
+        try:
+            _collection_path = collection_path if collection_path else cls.__name__
+            result = db.collection(_collection_path).document(doc_id).delete()
+        except Exception as e:
+            print(e)
+        return result
 
-  @classmethod
-  @require_database
-  def query(cls, q=()):
-    """ Get a handle to a query object (see Query helper class above)
-      @param cls The class of the instance calling make
-      @param q A list of query key/value or key/operator/value pairs (
-    """
-    return Query(cls, q)
+    @classmethod
+    @require_database
+    def get(cls, doc_id, collection_path: str = None, raise_exception=False):
+        """ Get a single model instance
+        @param cls
+            The class of the instance calling make
+        @param doc_id
+            The id of the document to get
+        @param collection_path
+            Override the default collection path
+        @return model
+            A model instance of type class hydrated w/ data from the database
+        """
+        try:
+            _collection_path = collection_path if collection_path else cls.__name__
+            doc_ref = db.collection(_collection_path).document(doc_id).get()
+            return cls(**doc_ref.to_dict())
+        except Exception as e:
+            if raise_exception:
+                raise e
+            return None
 
-  # --------------------------------------------
-  #
-  #  instance
-  #
-  # -------------------------------------------
+    @classmethod
+    def make(
+        cls,
+        doc_id: str = None,
+        collection_path: str = None,
+        save=False,
+        *args,
+        **kwargs
+    ):
+        """ Create a new instance of a model class
+        @param cls
+            The class of the instance calling make
+        @param doc_id
+            Allow create document with a custom id
+        @param collection_path
+            Override collection path
+        @param save
+            A flag indicating the model should be saved immediately after creation
+        @returns cls
+            A new model instance of type cls
 
-  id:str
-  created:int
-  modified:int
+        Examples:
+            User.make(
+                name = 'Sonic',
+                location = 'Earth',
+                save = True
+              )
 
-  @require_database
-  def delete(self, raise_exception=False):
-    """ Removes this model from Cloud Datastore
+            User.make(
+                doc_id = 'custom_id'
+                name = 'Sonic',
+                location = 'Earth',
+                collection_path = 'parent_collection',
+                save = True
+              )
+        """
+        id_str = doc_id if doc_id else str(uuid.uuid4())
+        created = get_milliseconds()
+        modified = created
+        _collection_path = collection_path if collection_path else cls.__name__
 
-      @raises Exception indicating that deletion failed
-    """
-    collection_name = self.__class__.__name__
-    try:
-      db.collection(collection_name).document(self.id).delete()
-      return True
-    except Exception as e:
-      if raise_exception:
-        raise e
-      print(e)
-      return False
+        model = cls(id_str, created, modified, _collection_path, *args, **kwargs)
 
-  @require_database
-  def save(self):
-    """ Saves this model to Cloud Firestore """
-    return self.set(asdict(self))
+        if save:
+            model.save()
 
-  @require_database
-  def set(self, kvs):
-    """ Set values on this model
-      @param kvs A dictionary containing key value pairs to set on this model.
-      Unrecognized keys are ignored
-    """
-    collection_name = self.__class__.__name__
-    for k, v in kvs.items():
-      if hasattr(self, k):
-        setattr(self, k, v)
-    self.modified = get_milliseconds()
-    db.collection(collection_name).document(self.id).set(asdict(self))
+        return model
 
-  def to_dict(self):
-    """ A convenience function that converts this model into a dictionary representation
+    @classmethod
+    @require_database
+    def query(cls, q=(), collection_path: str = None):
+        """ Get a handle to a query object (see Query helper class above)
+        @param cls
+            The class of the instance calling make
+        @param q
+            A list of query key/value or key/operator/value pairs
+        @param collection_path
+            Override collection path
+        """
+        return Query(cls, q, collection_path=collection_path)
 
-      @return Dictionary of key value pairs representing this model
-    """
-    return asdict(self)
+    # --------------------------------------------
+    #
+    #  instance
+    #
+    # -------------------------------------------
+
+    id: str
+    created: int
+    modified: int
+    collection_path: str
+
+    @require_database
+    def delete(self, raise_exception=False):
+        """ Removes this model from Cloud Datastore
+        @param raise_exception
+            whether to raise exception or boolean
+        @raises Exception or boolean
+            indicating that deletion failed
+        """
+        try:
+            db.collection(self.collection_path).document(self.id).delete()
+            return True
+        except Exception as e:
+            if raise_exception:
+                raise e
+            print(e)
+            return False
+
+    @require_database
+    def save(self):
+        """ Saves this model to Cloud Firestore """
+        return self.set(asdict(self))
+
+    @require_database
+    def set(self, kvs):
+        """ Set values on this model
+        @param kvs
+            A dictionary containing key value pairs to set on this model.
+
+        Unrecognized keys are ignored
+        """
+        for k, v in kvs.items():
+            if hasattr(self, k):
+                setattr(self, k, v)
+        self.modified = get_milliseconds()
+        db.collection(self.collection_path).document(self.id).set(asdict(self))
+
+    def to_dict(self):
+        """ A convenience function that converts this model into a dictionary
+        representation
+
+        @return Dict
+            A Dictionary of key value pairs representing this model
+        """
+        return asdict(self)
+
+    @require_database
+    def doc(self):
+        return db.collection(self.collection_path).document(self.id)
+
+    @require_database
+    def collection(self, collection_path):
+        return db.collection(collection_path)
